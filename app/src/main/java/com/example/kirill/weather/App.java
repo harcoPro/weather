81c17835b6e38b011bfb68ae201147e0f6e9c635
brulik <kirill.shpakovsky@raxeltelematics.com>
2017-05-17 11:52:46 +0600

    Dependencies have added in project

---
+++ app/src/main/java/com/example/kirill/weather/App.java
@@ -0,0 +1,15 @@
+package com.example.kirill.weather;
+
+import android.app.Application;
+
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+
+}
